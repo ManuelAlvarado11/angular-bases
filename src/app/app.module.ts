a7ffc791392a614e56a0645f6118813be3b06ab4
Manuel Alvarado <manuel.alvarado@bcr.gob.sv>
2023-10-10 09:05:56 -0600

    Format File Module`s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,10 @@ import { HeroesModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    CounterModule,
-    HeroesModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CounterModule, HeroesModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
